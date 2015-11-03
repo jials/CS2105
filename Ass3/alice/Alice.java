794de8737fa7dfd1a3acb250357caa16c6a73add
jials <jiale@comp.nus.edu.sg>
2015-11-03 08:28:11 +0800

    Initial Commit

---
+++ Ass3/alice/Alice.java
@@ -0,0 +1,130 @@
+// Author: 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.PublicKey;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SealedObject;
+
+/************************************************
+  * This skeleton program is prepared for weak  *
+  * and average students.                       *
+  * If you are very strong in programming. DIY! *
+  * Feel free to modify this program.           *
+  ***********************************************/
+
+// Alice knows Bob's public key
+// Alice sends Bob session (AES) key
+// Alice receives messages from Bob, decrypts and saves them to file
+
+class Alice {  // Alice is a TCP client
+    
+    String bobIP;  // ip address of Bob
+    int bobPort;   // port Bob listens to
+    Socket connectionSkt;  // socket used to talk to Bob
+    private ObjectOutputStream toBob;   // to send session key to Bob
+    private ObjectInputStream fromBob;  // to read encrypted messages from Bob
+    private Crypto crypto;        // object for encryption and decryption
+    // file to store received and decrypted messages
+    public static final String MESSAGE_FILE = "msgs.txt";
+    
+    public static void main(String[] args) {
+        
+        // Check if the number of command line argument is 2
+        if (args.length != 2) {
+            System.err.println("Usage: java Alice BobIP BobPort");
+            System.exit(1);
+        }
+        
+        new Alice(args[0], args[1]);
+    }
+    
+    // Constructor
+    public Alice(String ipStr, String portStr) {
+        
+        this.crypto = new Crypto();
+        
+        // Send session key to Bob
+        sendSessionKey();
+        
+        // Receive encrypted messages from Bob,
+        // decrypt and save them to file
+        receiveMessages();
+    }
+    
+    // Send session key to Bob
+    public void sendSessionKey() {
+        
+    }
+    
+    // Receive messages one by one from Bob, decrypt and write to file
+    public void receiveMessages() {
+        
+    }
+    
+    /*****************/
+    /** inner class **/
+    /*****************/
+    class Crypto {
+        
+        // Bob's public key, to be read from file
+        private PublicKey pubKey;
+        // Alice generates a new session key for each communication session
+        private SecretKey sessionKey;
+        // File that contains Bob' public key
+        public static final String PUBLIC_KEY_FILE = "public.key";
+        
+        // Constructor
+        public Crypto() {
+            // Read Bob's public key from file
+            readPublicKey();
+            // Generate session key dynamically
+            initSessionKey();
+        }
+        
+        // Read Bob's public key from file
+        public void readPublicKey() {
+            // key is stored as an object and need to be read using ObjectInputStream.
+            // See how Bob read his private key as an example.
+        }
+        
+        // Generate a session key
+        public void initSessionKey() {
+            // suggested AES key length is 128 bits
+        }
+        
+        // Seal session key with RSA public key in a SealedObject and return
+        public SealedObject getSessionKey() {
+            
+            // Alice must use the same RSA key/transformation as Bob specified
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            
+            // RSA imposes size restriction on the object being encrypted (117 bytes).
+            // Instead of sealing a Key object which is way over the size restriction,
+            // we shall encrypt AES key in its byte format (using getEncoded() method).           
+        }
+        
+        // Decrypt and extract a message from SealedObject
+        public String decryptMsg(SealedObject encryptedMsgObj) {
+            
+            String plainText = null;
+            
+            // Alice and Bob use the same AES key/transformation
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            
+            
+            return plainText;
+        }
+    }
+}
---
+++ Ass3/bryan/Bryan.java
@@ -0,0 +1,253 @@
+// Written by: Lifeng
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.GeneralSecurityException;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SealedObject;
+import javax.crypto.spec.SecretKeySpec;
+
+// Bryan receives AES session key from Amy
+// Bryan reads messages line by line from file
+// Bryan sends encrypted messages to Amy
+
+class Bryan {  // Bryan is a TCP server
+    
+    int port;  // port Bryan listens to
+    ServerSocket welcomeSkt;  // wait for Amy to connect
+    Socket connectionSkt;     // socket used to talk to Amy
+    private ObjectInputStream fromAmy;  // to read session key from Amy
+    private ObjectOutputStream toAmy;   // to send encrypted messages to Amy
+    private Crypto crypto;    // for encryption and decryption
+    // file that contains messages to send
+    public static final String MESSAGE_FILE = "docs.txt";
+    
+    public static void main(String[] args)  {
+        
+        // Check if the number of command line argument is 1
+        if (args.length != 1) {
+            System.err.println("Usage: java Bryan BryanPort");
+            System.exit(1);
+        }
+        
+        new Bryan(args[0]);
+    }
+    
+    // Constructor
+    public Bryan(String portStr) {
+        
+        this.crypto = new Crypto();
+        
+        this.port = Integer.parseInt(portStr);
+        
+        try {
+            this.welcomeSkt = new ServerSocket(this.port);
+        } catch (IOException ioe) {
+            System.out.println("Error creating welcome socket");
+            System.exit(1);
+        }
+        
+        System.out.println("Bryan listens at port " + this.port);
+        
+        // Create a separate socket to connect to a client
+        try {
+            this.connectionSkt = this.welcomeSkt.accept();
+        } catch (IOException ioe) {
+            System.out.println("Error creating connection socket");
+            System.exit(1);
+        }
+        
+        try {
+            this.toAmy = new ObjectOutputStream(this.connectionSkt.getOutputStream());
+            this.fromAmy = new ObjectInputStream(this.connectionSkt.getInputStream());
+        } catch (IOException ioe) {
+            System.out.println("Error: cannot get input/output streams");
+            System.exit(1);
+        }
+
+    // Send public key and digest
+    sendPublicKey();
+        
+        // Receive session key from Amy
+        getSessionKey();
+        
+        // Send encrypted messages to Amy
+        sendMessages();
+        
+        // Clean up
+        try {
+            this.welcomeSkt.close();
+            this.connectionSkt.close();
+        } catch (IOException ioe) {
+            System.out.println("Error closing TCP sockets");
+            System.exit(1);
+        }
+    }
+
+    // Send public key to Amy
+    public void sendPublicKey() {
+        try {
+            ObjectInputStream ois = 
+                    new ObjectInputStream(new FileInputStream("bryan.pub"));
+            PublicKey pubKey = (PublicKey)ois.readObject();
+            ois.close();
+
+            byte[] digest = Files.readAllBytes(Paths.get("bryan.sig"));
+
+            this.toAmy.writeObject(pubKey);
+            this.toAmy.writeObject(digest);
+        } catch (IOException ioe) {
+            System.out.println("Error sending public key to Amy");
+            System.exit(1);
+        } catch (ClassNotFoundException ioe) {
+            System.out.println("Error: cannot typecast to class PublicKey");
+            System.exit(1); 
+        }
+    }
+
+    
+    // Receive session key from Amy
+    public void getSessionKey() {
+        
+        try {
+            SealedObject sessionKeyObj = (SealedObject)this.fromAmy.readObject();
+            this.crypto.setSessionKey(sessionKeyObj);
+        } catch (IOException ioe) {
+            System.out.println("Error receiving session key from Amy");
+            System.exit(1);
+        } catch (ClassNotFoundException ioe) {
+            System.out.println("Error: cannot typecast to class SealedObject");
+            System.exit(1); 
+        }
+    }
+    
+    // Read messages one by one from file, encrypt and send them to Amy
+    public void sendMessages() {
+        
+        try {
+            
+            Scanner fromFile = new Scanner(new File(MESSAGE_FILE));
+            
+            // Assume there are exactly 10 lines to read
+            for (int i = 0; i < 10; i++) {
+                String message = fromFile.nextLine();
+                SealedObject encryptedMsg = this.crypto.encryptMsg(message);
+                this.toAmy.writeObject(encryptedMsg);
+            }
+            
+            fromFile.close();  // close input file stream
+            System.out.println("All 10 messages are sent to Amy");
+            
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Error: " + MESSAGE_FILE + " doesn't exist");
+            System.exit(1);
+        } catch (IOException ioe) {
+            System.out.println("Error sending messages to Amy");
+            System.exit(1);
+        }
+    }
+    
+    /*****************/
+    /** inner class **/
+    /*****************/
+    class Crypto {
+        
+        // Use the same RSA private keys for all sessions.
+        private PrivateKey privKey;  // private key is a secret
+        // Session key is received from Amy
+        private SecretKey sessionKey;
+        // File that contains private key
+        public static final String PRIVATE_KEY_FILE = "bryan.pri";
+        
+        // Constructor
+        public Crypto() {
+            
+            // read private key from file
+            File privKeyFile = new File(PRIVATE_KEY_FILE);
+            if ( privKeyFile.exists() && !privKeyFile.isDirectory() ) {
+                readPrivateKey();
+            } else {
+                System.out.println("Bryan cannot find RSA private key.");
+                System.exit(1);
+            }
+        }
+        
+        // Receive a session key from Amy
+        public void setSessionKey(SealedObject sessionKeyObj) {
+            
+            try {
+                // getInstance(crypto algorithm/feedback mode/padding scheme)
+                // Amy will use the same key/transformation
+                Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+                cipher.init(Cipher.DECRYPT_MODE, this.privKey);
+                
+                // receive an AES key in "encoded form" from Amy
+                byte[] rawKey = (byte[])sessionKeyObj.getObject(cipher);
+                // reconstruct AES key from encoded form
+                this.sessionKey = new SecretKeySpec(rawKey, 0, rawKey.length, "AES");
+            } catch (GeneralSecurityException gse) {
+                System.out.println("Error: wrong cipher to decrypt session key");
+                System.exit(1);
+            } catch (IOException ioe) {
+                System.out.println("Error receiving session key");
+                System.exit(1);
+            } catch (ClassNotFoundException ioe) {
+                System.out.println("Error: cannot typecast to byte array");
+                System.exit(1); 
+            }
+        }
+        
+        // Encrypt a message and encapsulate it as a SealedObject
+        public SealedObject encryptMsg(String msg) {
+            
+            SealedObject sessionKeyObj = null;
+            
+            try {
+                // getInstance(crypto algorithm/feedback mode/padding scheme)
+                // Amy will use the same key/transformation
+                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, sessionKey);
+                sessionKeyObj = new SealedObject(msg, cipher);
+            } catch (GeneralSecurityException gse) {
+                System.out.println("Error: wrong cipher to encrypt message");
+                System.exit(1);
+            } catch (IOException ioe) {
+                System.out.println("Error creating SealedObject");
+                System.exit(1);
+            }
+            
+            return sessionKeyObj;
+        }
+        
+        // Read private key from a file
+        public void readPrivateKey() {
+            
+            try {
+                ObjectInputStream ois = 
+                    new ObjectInputStream(new FileInputStream(PRIVATE_KEY_FILE));
+                this.privKey = (PrivateKey)ois.readObject();
+                ois.close();
+            } catch (IOException oie) {
+                System.out.println("Error reading private key from file");
+                System.exit(1);
+            } catch (ClassNotFoundException cnfe) {
+                System.out.println("Error: cannot typecast to class PrivateKey");
+                System.exit(1);
+            }
+            
+            System.out.println("Private key read from file " + PRIVATE_KEY_FILE);
+        }
+    }
+}
